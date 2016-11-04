2669ec5d214fe4d0b7b888e6d03aaccf85f671ec
sebagomez <sebagomezcorrea@outlook.com>
2016-11-04 17:46:39 -0300

    meto el back en el botón Cancel

--- src/components/cliente/cliente.jsx
+++ src/components/cliente/cliente.jsx
@@ -1,9 +1,9 @@
 import "./cliente.css";
 import React from "react";
-import { Grid, Form, Header, Icon, Button, Label, Input} from 'semantic-ui-react'
+import { Grid, Form, Header, Icon, Button, Label, Input} from 'semantic-ui-react';
+import { browserHistory  } from 'react-router';
 
 function template() {
-debugger;
   return (
   <div className="container">
     <Header as='h2' icon>
@@ -16,6 +16,7 @@ debugger;
       <Form className="form-horizontal">
         <Form.Input id="CLIENTID" defaultValue={this.props.id} size="mini" type='text' label="Id" placeholder='Id' />
         <Form.Input id="CLIENTNAME" defaultValue="" size="small" type='text' label="Nombre" placeholder='Nombre' />
+        <img id="CLIENTIMAGE" src={this.props.image} alt="Player's picture" />
         <Form.Input labelPosition='right'>
           <Label basic>$</Label>
             <Input id="CLIENTBALANCE" label="Saldo" type='small' placeholder='Saldo' />
@@ -24,7 +25,7 @@ debugger;
         <Grid>
         <Grid.Row centered>
         <Button.Group>
-          <Button>Cancel</Button>
+          <Button onClick={browserHistory.goBack} >Cancel</Button>
           <Button.Or />
           <Button positive>Guardar</Button>
         </Button.Group>
