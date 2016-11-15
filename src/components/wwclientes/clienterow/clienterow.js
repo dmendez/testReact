e8a64097dd0cabd07a8078c83a67174af48f81ce
dmendez <dmendez@genexus.com>
2016-11-15 15:47:35 -0300

    falto un fuente en commit anterior (Sincronizado a mano con beyond [mac/win])

--- src/components/wwclientes/clienterow/clienterow.js
+++ src/components/wwclientes/clienterow/clienterow.js
@@ -1,10 +1,7 @@
 import React from "react";
 import template from "./clienterow.jsx";
-
 import * as actions from '../../../actions';
-
-import configureStore from '../../../store/configureStore';
-const store = configureStore();
+import { browserHistory } from 'react-router';
 
 class clienterow extends React.Component {
   render() {
@@ -12,14 +9,19 @@ class clienterow extends React.Component {
   }
 
   editClient(evt, cliente) {
-      store.dispatch(actions.editCliente( evt, cliente));
-      store.dispatch(actions.navigateCliente( evt));
+    let store = this.context.store;
+    browserHistory.push('/cliente');
+    store.dispatch(actions.editCliente( evt, cliente));
   }
 
   deleteClient(evt, cliente) {
-      store.dispatch(actions.deleteCliente( evt, cliente));
-      store.dispatch(actions.navigateCliente( evt));
+      browserHistory.push('/cliente');
+      this.props.store.dispatch(actions.deleteCliente( evt, cliente));
   }
 }
+
+clienterow.contextTypes = {
+  store: React.PropTypes.object
+};
 
 export default clienterow;
