d835b3d49f6cc7a420c7d782d9cdbe0d16d4c5c4
sebagomez <sebagomezcorrea@outlook.com>
2016-11-25 11:10:23 -0300

    Redux DevTools usage added according to: https://github.com/zalmoxisus/redux-devtools-extension#usage

--- src/store/configureStore.dev.js
+++ src/store/configureStore.dev.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { apiMiddleware } from 'redux-api-middleware';
 import createLogger from 'redux-logger';
@@ -6,7 +6,9 @@ import rootReducer from '../reducers';
 
 export default function configureStore(preloadedState) {
   let middleware = [thunk, apiMiddleware, createLogger()];
-  const store = applyMiddleware(...middleware)(createStore)(rootReducer, preloadedState);
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+  const store = createStore(rootReducer, preloadedState, composeEnhancers (applyMiddleware(...middleware)));
   if (module.hot) {
     // Enable Webpack hot module replacement for reducers
     module.hot.accept('../reducers', () => {
